f5efe981b44be8749110dff2040e589f7fe07bfd
RanjithSrikanth <ranjith2112052@ssn.edu.in>
2024-06-18 16:06:59 +0530

    commit4
    c

--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Doughnut } from 'vue-chartjs'
 import * as chartConfig from './chartConfig.js'
@@ -37,7 +37,11 @@ export default {
     </div>
     <div class="stt">
         <h4>Scheduled Jobs</h4>
-        <h5>Archival of TN tender docs</h5>
+        
+        <div>
+          <h4>Archival of TN tender docs</h4>
+        </div>
+
     </div>
 </div>
 
@@ -72,5 +76,130 @@ margin-right: 50px;
   height: 200px;
   width: 200px;
   margin-bottom: 10px;
+}
+</style> -->
+
+<script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Doughnut
+  },
+  data() {
+    return chartConfig
+  },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      default: () => ({ responsive: true, maintainAspectRatio: false })
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="containerr">
+    <div class="storage-available">
+      <h4>Storage Available</h4>
+      <div id="storage">
+        <Doughnut class="pie" :data="data" />
+      </div>
+    </div>
+    <div class="scheduled-jobs">
+      <h4>Scheduled Jobs</h4>
+      <div class="job">
+        <a href="#">Archival of TN Tender Docs</a>
+        <button class="details">Details</button>
+        <button class="reschedule">Reschedule</button>
+        <button class="abort">Abort</button>
+      </div>
+      <div class="job">
+        <a href="#">Archival of DNH Tender Docs</a>
+        <button class="details">Details</button>
+        <button class="reschedule">Reschedule</button>
+        <button class="abort">Abort</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+h4 {
+  margin-top: 6px;
+  margin-bottom: 6px;
+  margin-left: 3px;
+}
+.storage-available {
+  border: 2px solid black;
+  border-radius: 15px;
+  margin-left: 50px;
+  margin-right: 10px;
+}
+.scheduled-jobs {
+  border: 2px solid black;
+  border-radius: 15px;
+  margin-left: 40px;
+  margin-right: 10px;
+  padding: 10px;
+}
+#containerr {
+  display: flex;
+  flex-wrap: wrap;
+}
+#storage {
+  height: 200px;
+  width: 200px;
+  margin-bottom: 10px;
+}
+.job {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.job a {
+  color: blue;
+  text-decoration: none;
+  flex-grow: 1;
+}
+.details,
+.reschedule,
+.abort {
+  background-color: lightgray;
+  border: none;
+  border-radius: 5px;
+  padding: 5px 10px;
+  width: 100px; /* Fixed width */
+  text-align: center;
+  box-sizing: border-box;
+  margin-right: 10px;
+}
+.abort {
+  background-color: red;
+  color: white;
+}
+
+/* Responsive adjustments */
+@media (max-width: 600px) {
+  .storage-available,
+  .scheduled-jobs {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .details,
+  .reschedule,
+  .abort {
+    width: auto;
+    flex: 1; /* Allow buttons to grow and shrink as needed */
+  }
 }
 </style>
